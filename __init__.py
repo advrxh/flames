cfbb46612ce6f784bded86f39224984c5d72b0a5
AadilVarsh <aadilvarshofficial@gmail.com>
2021-04-20 20:32:41 +0530

    fixed logical loop holes

--- __init__.py
+++ __init__.py
@@ -7,40 +7,55 @@ class Flames:
     """
 
     def __init__(self, name_a, name_b):
-        self.name_a = name_a
-        self.name_b = name_b
+        '''
+            __init__
+            :function computes two names:
+            :param_1 name_a : first names
+            :param_2 name_b : second name
+        '''
 
-        self.flames = "FLAMES"
+        if name_a != name_b:
+            self.name_a = name_a.strip().replace('.', str()).lower()
+            self.name_b = name_b.strip().replace('.', str()).lower()
 
-        self.tags = {
-            'f': 'friends',
-            'l': 'lovers',
-            'a': 'affection',
-            'm': 'marriage',
-            'e': 'enemies',
-            's': 'siblings',
-        }
+            self.flames = "FLAMES".lower()
 
-        _name_a, _name_b = self.name_a, self.name_b
+            self.tags = {
+                'f': 'friends',
+                'l': 'lovers',
+                'a': 'affection',
+                'm': 'marriage',
+                'e': 'enemies',
+                's': 'siblings',
+            }
 
-        for char_a in _name_a:
-            for char_b in _name_b:
-                if char_a == char_b:
-                    _name_a.replace(char_a, str())
-                    _name_b.replace(char_b, str())
+            _name_a, _name_b = self.name_a, self.name_b
 
-        len_no = len(_name_a) + len(_name_b)
+            for char_a in _name_a:
+                for char_b in _name_b:
+                    if char_a == char_b:
+                        _name_a.replace(char_a, str())
+                        _name_b.replace(char_b, str())
 
-        rem = len_no % 6
+            len_no = len(_name_a) + len(_name_b)
 
-        while rem > 6:
-            rem = rem % 6
+            rem = len_no % 6
 
-        self.flame = self.flames[rem - 1]
+            while rem > 6:
+                rem = rem % 6
+
+            self.flame = self.flames[rem - 1]
+        else:
+            raise Exception('Names cannot be same!')
 
     def find_it(self):
+        '''
+            find_it
+            :function returns flame tag:
+            :params: none
+        '''
 
-        return self.flame
+        return self.tags[self.flame.lower()].lower()
 
 
 if __name__ == "__main__":
